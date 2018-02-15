9b317d9181cac93000708d12a066e03e29a76f1f
Timothy M Owen <timothy.m.owen@nga.mil>
2018-02-15 11:04:05 +0900

    added services for hero and messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { FormsModule } from '@angular/forms'; // <- used for forms module
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ 
+    HeroService, 
+    MessageService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -11,22 +11,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  //hero: Hero = {
-  //  id: 1,
-  //  name: 'Windstorm'
-  //};
-
-  heroes = HEROES;
-
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
